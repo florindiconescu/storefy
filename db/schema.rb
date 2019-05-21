35b5a70b5dc1851e797813ae6d643dd8dc10cde1
Florin Diconescu <florin.diconescu@gmail.com>
2019-05-21 16:52:43 +0200

    added photo uploader

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_160452) do
+ActiveRecord::Schema.define(version: 2019_05_21_140900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,8 +60,8 @@ ActiveRecord::Schema.define(version: 2019_05_20_160452) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.boolean "owner", default: false
-    t.string "avatar"
+    t.boolean "owner"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
