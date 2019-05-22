6d44970f1e97dc2634d92198cb7e2c076af6ef04
Florin Diconescu <florin.diconescu@gmail.com>
2019-05-22 12:46:44 +0200

    adding mapbox

--- db/schema.rb
+++ db/schema.rb
@@ -9,8 +9,8 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 2019_05_22_095619)
-ActiveRecord::Schema.define(version: 2019_05_22_101339)
+
+ActiveRecord::Schema.define(version: 2019_05_22_101339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
