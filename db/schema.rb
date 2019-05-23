b66090afff532442454fed666f6d70b550cb9f45
David Soutoul <david.soutoul@gmail.com>
2019-05-23 11:19:13 +0200

    finished design css for homepage

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_22_105858)
-ActiveRecord::Schema.define(version: 2019_05_22_101339)
+ActiveRecord::Schema.define(version: 2019_05_22_105858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
